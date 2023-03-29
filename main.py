c471d524c355fcf9788404b7b899c78d4983d379
neyen <neyen.gmbh@gmail.com>
2023-03-29 22:51:39 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+import pandas
+import smtplib
+import datetime
+import random
+
+my_email = "minhphi.nguyen91@gmail.com"
+password ="gkytuckgjfesrtlh"
+
+# 2. Check if today matches a birthday in the birthdays.csv
+today= datetime.datetime.now()
+today_tuple = (today.month, today.day)
+
+data = pandas.read_csv("birthdays.csv")
+
+new_dict ={(data_row["month"], data_row["day"]): data_row for(index, data_row) in data.iterrows()}
+
+if today_tuple in new_dict:
+    birthday_person = new_dict[today_tuple]
+    file_path = f"letter_templates/letter_{random.randint(1,3)}.txt"
+    with open(file_path) as letter_file:
+        contents = letter_file.read()
+        contents = contents.replace("[NAME]", birthday_person["name"])
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=my_email,password=password)
+        connection.sendmail(from_addr=my_email,
+                            to_addrs=birthday_person["email"],
+                            msg=f"Subject: Happy Birthday! \n\n{contents}")
+
+
+
+
+
